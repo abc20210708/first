1e4449ef395b291f3077374a63d83eca18b6cb7f
abc20210708 <gotqlc2670@naver.com>
2022-08-26 14:29:47 +0900

    Create CategoryMapper.xml

--- src/main/java/com/shop/first/category/service/CategoryService.java
+++ src/main/java/com/shop/first/category/service/CategoryService.java
@@ -1,6 +1,21 @@
 package com.shop.first.category.service;
 
+import com.shop.first.category.domain.Category;
+import com.shop.first.category.repository.CategoryMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service @Log4j2
+@RequiredArgsConstructor
 public class CategoryService {
 
+    private final CategoryMapper categoryMapper;
 
+    //카테고리 조회 중간처리
+    public List<Category> getCategory() {
+        return categoryMapper.getCategory();
+    }
 }
