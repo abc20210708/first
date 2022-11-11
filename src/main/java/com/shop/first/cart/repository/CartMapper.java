d597caafd19cd5bf42c56187b11a24e28916f4f0
abc20210708 <gotqlc2670@naver.com>
2022-11-11 13:28:41 +0900

    Update CartMapper.java

--- src/main/java/com/shop/first/cart/repository/CartMapper.java
+++ src/main/java/com/shop/first/cart/repository/CartMapper.java
@@ -32,6 +32,18 @@ public interface CartMapper {
 
     //장바구니 동일한 상품 확인
     int countCart(@Param("csId") String csId, @Param("prCode") int prCode);
+    /*
+    @Param
+    데이터베이스에 다수의 변수를 전달할 때는 전달되는 변수들에 @Param 어노테이션을
+    붙여주어 각 변수를 구분할 수 있도록한다. 변수를 한개만 전달할 할때는 상관없지만,
+    다수의 변수를 전달할때는 반드시 써주는게 좋다.
+
+    참고 블로그
+    https://velog.io/@alicesykim95/Pathvariable-RequestParam-PathParam-Pram
+    */
+
+    //@RequestParam
+    //참고 블로그 https://m42-orion.tistory.com/114?category=1085496
 
     //장바구니 수량 수정
     void modifyCart(ModCart cart);
