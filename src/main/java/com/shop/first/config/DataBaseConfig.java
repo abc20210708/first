20c70e63a27f5f1a781cf2e02a71f334017fd301
abc20210708 <gotqlc2670@naver.com>
2023-06-15 21:35:45 +0900

    Update DataBaseConfig.java

--- src/main/java/com/shop/first/config/DataBaseConfig.java
+++ src/main/java/com/shop/first/config/DataBaseConfig.java
@@ -26,3 +26,14 @@ public class DataBaseConfig {
     }
 
 }//
+
+/*
+component-scan 이란?
+
+빈으로 등록 될 준비를 마친 클래스들을 스캔하여, 빈으로 등록해주는 것이다.
+빈으로 등록 될 준비를 하는 것이 무엇일까?
+우리가 @Controller, @Service, @Component, @Repository 어노테이션을 붙인
+클래스들이 빈으로 등록 될 준비를 한 것이다.
+
+참고 블로그 https://velog.io/@hyun-jii/%EC%8A%A4%ED%94%84%EB%A7%81-component-scan-%EA%B0%9C%EB%85%90-%EB%B0%8F-%EB%8F%99%EC%9E%91-%EA%B3%BC%EC%A0%95
+* */
